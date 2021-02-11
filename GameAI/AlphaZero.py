3c46ec5d2ddb80d3630a9bdf9c56d19b93714d69
Josiah Ross <jojo1023@users.noreply.github.com>
2021-02-10 17:42:22 -0800

    Worked on NN

--- GameAI/AlphaZero.py
+++ GameAI/AlphaZero.py
@@ -1,4 +1,5 @@
 from Mcts import *
+import keras
 from keras.models import Model
 from keras.models import Sequential
 from keras.layers import Dense
@@ -7,17 +8,24 @@ from keras.layers import Input
 from keras.layers import concatenate
 from keras.utils import plot_model
 import pydot
+import os.path
+from os import path
+from keras.optimizers import Adam
+from keras.optimizers import SGD
+
 
 class GameStateStorage:
     def __init__(self, currentPlayer, gameState, nextMoveProbs):
         self.currentPlayer = currentPlayer
         self.gameState = gameState
         self.nextMoveProbs = nextMoveProbs
-        self.value = 0 # relative to current player
+        self.value = 0  # relative to current player
+
 
 class AlphaZero:
     def __init__(self, config: GameAiConfig):
         self.config = config
+        self.currentNNPath = "currentNNPath.txt"
 
     def Start(self, game):
         gameGameStates = []
@@ -27,10 +35,10 @@ class AlphaZero:
 
         self.TrainNN(gameGameStates)
 
-    def PlayGame(self, game : Game, allMoves):
+    def PlayGame(self, game: Game, allMoves):
         mcts = Mcts(self.config, False)
         gameStates = []
-        while not game.IsTerminal(): # add max length
+        while not game.IsTerminal():  # add max length
             cp = game.GetCurrentPlayer()
             (a, probsDict) = mcts.runSims(game, addNoise=True)
             gs = game.GetBoardState(cp)
@@ -48,9 +56,26 @@ class AlphaZero:
             gs.value = (gameValue if gs.currentPlayer else -gameValue)
         return gameStates
 
+    def TrainNN(self, gameGameStates: list[list[GameStateStorage]]):
+        if len(gameGameStates) <= 0 or len(gameGameStates[0]) <= 0:
+            return
+        model = self.GetNewNN(len(gameGameStates[0][0].gameState), len(gameGameStates[0][0].nextMoveProbs))
+        if path.exists(self.currentNNPath):
+            currentNNWeightsFilePath = open(self.currentNNPath, "w+")
+        else:
+            currentNNWeightsFilePath = open(self.currentNNPath, "a+")
 
-    def TrainNN(self, gameGameStates):
+        opt = SGD(learning_rate=self.config.learning_rate_schedule[0], momentum=self.config.momentum)
+        lrScheduleCallback = keras.callbacks.LearningRateScheduler(self.config.learning_rate_schedule)
+
+        model.compile(optimizer=opt, loss={'valueOutput': 'mse', 'policyOutput': 'categorical_crossentropy'},
+                      metrics={'valueOutput': 'mae', 'policyOutput': 'accuracy'})
+
+        model.train_on_batch(x=sdf, y=dsf)
+        currentNNWeightsFilePath.close()
+    def GetNNBatch(self, gameGameStates: list[list[GameStateStorage]]):
         pass
+
 
     def GetNewNN(self, inputCount, outputCount):
         inL = Input(shape=(inputCount,))
@@ -62,13 +87,13 @@ class AlphaZero:
 
         hiddenV = Dense(15, activation='tanh')(hidden)
         hiddenV = Dense(5, activation='tanh')(hiddenV)
-        outV = Dense(1, activation='tanh')(hiddenV)
+        outV = Dense(1, activation='tanh', name='valueOutput')(hiddenV)
 
         hiddenP = Dense(30, activation='relu')(hidden)
         hiddenP = Dense(30, activation='tanh')(hiddenP)
-        outP = Dense(outputCount, activation='softmax')(hiddenP)
+        outP = Dense(outputCount, activation='softmax', name='policyOutput')(hiddenP)
 
-        model = Model(inputs = inL, outputs=[outV, outP])
+        model = Model(inputs=inL, outputs=[outV, outP])
         print(model.summary())
         # plot_model(model, to_file='multiple_outputs.png')
         return model
@@ -76,4 +101,5 @@ class AlphaZero:
 
 az = AlphaZero(GameAiConfig())
 db = DotsAndBoxes(3)
+az.TrainNN(None)
 az.GetNewNN(len(db.GetBoardState(True)), len(db.GetAllPossibleMoves()))
--- GameAI/GameAiConfig.py
+++ GameAI/GameAiConfig.py
@@ -3,5 +3,13 @@ class GameAiConfig:
         self.numSimulations = 4000
         self.usbExploreBase = 19652
         self.usbExploreInit = 1.25
-        self.root_dirichlet_alpha = 0.3
+        self.root_dirichlet_alpha = 0.5 # 0.3 for chess
+
         self.root_exploration_fraction = 0.25
+        self.momentum = 0.9
+        self.learning_rate_schedule = {
+            0: 0.02,
+            100000: 0.002,
+            300000: 0.0002,
+            500000: 0.0002
+        }
